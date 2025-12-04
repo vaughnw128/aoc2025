6e97deeaec72c786c9343649975b7356e6e1b9d7
Vaughn Woerpel <vaughnwoerpel@gmail.com>
2025-12-04 01:03:04 -0500

    beat four

---
+++ three/src/main.rs
@@ -0,0 +1,16 @@
+// https://adventofcode.com/2025/day/2
+
+use std::fs::read_to_string;
+
+
+fn main() {
+    // let args = env::args().collect::<Vec<String>>();
+    // let input = &args[1];
+
+    // I fucked it up lol, got one star... will come back to this!
+
+    for line in read_to_string("input").unwrap().lines() {
+        let mut digits = line.chars().collect::<Vec<char>>();
+    }
+
+}
